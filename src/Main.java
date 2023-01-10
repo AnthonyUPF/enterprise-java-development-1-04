0f7364b37e7e7fef67b8808f00765435388623f7
AnthonyUPF <anthony.torres.h@gmail.com>
2023-01-10 16:20:49 +0100

    primer metodo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] arrayIntegers={10,3,4,5};
+
+        difference(arrayIntegers);
+
+
     }
+    public static void difference(int [] array){
+        int min=array[0];
+        int max=array[0];
+        for(int n: array){
+            if(n<=min){
+                min=n;
+            }else if(n>max){
+                max=n;
+            }
+        }
+        System.out.println("Minimum: "+min);
+        System.out.println("maximum: "+max);
+        System.out.println("Difference: "+ (max-min));
+    }
+
+
 }
